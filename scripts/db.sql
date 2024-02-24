069ca02cb9f2349bffe061b400996e7fcdd5ec40
Lucas Carnizella <lucas.carnizella@inova.ind.br>
2024-02-24 19:36:58 -0300

    Altered db script to be able to initialize database via docker entrypoint

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,13 +1,25 @@
-CREATE DATABASE marketplace
-    WITH
-    OWNER = postgres
+CREATE ROLE market_user WITH
+  LOGIN
+  NOSUPERUSER
+  NOCREATEROLE
+  NOREPLICATION;
+
+ALTER USER market_user WITH 
+    ENCRYPTED PASSWORD 'dev123';
+
+CREATE DATABASE marketplace WITH
+    OWNER = master
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
     LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TEMPLATE = template0
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+GRANT ALL PRIVILEGES ON DATABASE marketplace TO market_user;
+
+\connect marketplace;
 
 CREATE TABLE products (
 	id SERIAL PRIMARY KEY,
