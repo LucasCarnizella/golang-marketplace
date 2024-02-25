6040d91febdcc91cd84d98a1efd2174599094e2c
Lucas Carnizella <lucascarnizella035@hotmail.com>
2024-02-25 11:32:10 -0300

    Refactor main.go to read env variables and query products from the database

--- main.go
+++ main.go
@@ -2,46 +2,134 @@ package main
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
 	"text/template"
 
 	_ "github.com/lib/pq"
 )
 
 type Product struct {
+	ID          int
 	Name        string
 	Description string
 	Price       float64
-	Quantity    int
+	Amount      int
 }
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	appHost := os.Getenv("APP_HOST")
+
 	db := dbConnection()
-	defer db.Close()
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+
+	// To-Do handle http & https urls
+	err = openUrl("http://" + appHost)
+
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+	err = http.ListenAndServe(appHost, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection()
 
-	//	products := []Product{
-	//		{Name: "T-Shirt", Description: "Black", Price: 49.99, Quantity: 30},
-	//		{Name: "Snicker", Description: "Size 20", Price: 109.99, Quantity: 10},
-	//	}
+	selectAllProducts, err := db.Query("SELECT * FROM products")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	templates.ExecuteTemplate(w, "Index", nil)
+	p := Product{}
+	var products []Product
+
+	for selectAllProducts.Next() {
+		var id int
+		var name string
+		var description string
+		var price float64
+		var amount int
+
+		err = selectAllProducts.Scan(&id, &name, &description, &price, &amount)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		p.ID = id
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Amount = amount
+
+		products = append(products, p)
+	}
+
+	err = templates.ExecuteTemplate(w, "Index", products)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
 }
 
 func dbConnection() *sql.DB {
-	connStr := "dbname=marketplace user=postgres password=inova2023 host=localhost port=5432 sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	connStr := "dbname=" + dbName + " user=" + dbUser + " password=" + dbPassword + " host=" + dbHost + " port=" + dbPort + " sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return db
+}
+
+func openUrl(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
